d3f1a4e9c8ef6f7036f42f6dc16c0f56239eb108
noel <humblewolfstudio@gmail.com>
2024-02-05 12:20:05 +0100

    installation file done?

--- installation.sh
+++ installation.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-# Define variables
+unix_installation() {
+    curl -L "$EXECUTABLE" > "$INSTALL_DIR"
+
+    chmod a+x "$INSTALL_DIR"
+
+    if ! grep -q "$INSTALL_DIR" ~/.bash_profile; then
+        echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bash_profile
+        source ~/.bash_profile
+    fi
+}
+
+windows_installation() {
+    curl -L "$EXECUTABLE" > "$INSTALL_DIR"
+
+    if ! grep -q "$INSTALL_DIR" ~/.bash_profile; then
+        echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bash_profile
+        source ~/.bash_profile
+    fi
+}
+
 INSTALL_DIR="/usr/local/bin/doclite"
 EXECUTABLE="https://github.com/humblewolfstudio/doc-lite/releases/download/v0.1/doclite"
 
-# Copy the executable to the installation directory
-curl -L "$EXECUTABLE" > "$INSTALL_DIR"
+os_name=$(uname)
 
-chmod a+x "$INSTALL_DIR"
-
-if ! grep -q "$INSTALL_DIR" ~/.bash_profile; then
-    # Add the installation directory to the PATH in the user's shell profile
-    echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bash_profile
-    # Source the user's shell profile to apply the changes
-    source ~/.bash_profile
+# Check the operating system
+if [[ "$os_name" == "Linux" ]]; then
+    unix_installation
+elif [[ "$os_name" == "Darwin" ]]; then
+    unix_installation
+elif [[ "$os_name" == "CYGWIN"* || "$os_name" == "MINGW32"* || "$os_name" == "MSYS"* ]]; then
+    windows_installation
+else
+    echo "Unknown operating system"
 fi
 
-echo "Installation complete. $EXECUTABLE is now in your PATH."
+echo "Installation complete. doclite is now in your PATH."
